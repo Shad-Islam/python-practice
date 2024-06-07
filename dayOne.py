1005589f584a41816fa40c483542c4305e105bf2
Shad-islam <m.shadislam01@gmail.com>
2024-06-08 05:55:56 +0600

    First day practice

---
+++ dayOne.py
@@ -0,0 +1,28 @@
+# To check the Python version of the editor
+# import sys
+# print(sys.version)
+
+#indentation
+# if 5 > 2:
+#     print("Five is greater than two")
+
+#get the type
+# x = 5
+# print(type(x))
+# print (type(str(x)))
+
+#unpacking
+# fruits = ['Apple', 'Banana', 'Cherry']
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+#local variables
+# x = "great"
+# def myFuction():
+#     x = "Fantastic"
+#     print("Python is " + x)
+# myFuction()
+#
+# print("Python is " + x)
