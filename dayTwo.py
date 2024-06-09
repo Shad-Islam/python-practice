73ab3e6a4484ec71dabbb14323f292c53d13918a
Shad-islam <m.shadislam01@gmail.com>
2024-06-09 06:22:40 +0600

    Add day two

---
+++ dayTwo.py
@@ -0,0 +1,23 @@
+#Random number
+# import random
+# num = random.randrange(1,10)
+# print(num)
+
+# Multiline strings use """  """or ''' '''
+# a = """hello world
+# hello world
+# hello world"""
+# print(a)
+
+# String length
+# a = "Hello world"
+# print (len(a))
+
+# Check string
+# text = "Hello everyone , how are you?"
+# print("you" in text)
+
+# Use it in an if statement:
+# text = "Hello everyone , how are you?"
+# if "you" in text:
+#     print("You is present in the text")
